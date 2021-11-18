ced68e2a22d400bf1fd24c69a4fec77689c21750
PseudoHUEry <harry-kevin@hotmail.com>
2021-11-18 11:17:26 -0300

    feature

--- server/controllers/dashController/dashController.js
+++ server/controllers/dashController/dashController.js
@@ -3,16 +3,36 @@ const fs = require("fs"),
   expenseBd = fs.readFileSync(
     path.join(__dirname, "..", "..", "bd", "expensesBd.JSON")
   ),
+  userBd = fs.readFileSync(path.join(__dirname, "..", "..", "bd", "user.JSON")),
   expenses = JSON.parse(expenseBd),
-
-  dashbordController = {
-    index: (req, res, next) => 
-      res.render("dashboard", {
-        title: "Dashboard",
-        style: "stylesheets/dashboard.css",
-        bd: ""
-      })
-
+  users = JSON.parse(userBd),
+  Item = function (user, value) {
+    this.user = user;
+    this.value = value;
   };
+
+dashbordController = {
+  index: (req, res, next) => {
+    let item = [],
+      user = users.map((x) => {
+        let { user } = x;
+        return user;
+      }),
+      arr = user.map((y) => {
+        let ex = expenses.filter((x) => x.createBy == y),
+          total = ex.reduce((total, x) => {
+            return (total = total + x.value);
+          }, 0);
+        let js = new Item(y, total);
+        item.push(js);
+      });
+
+    res.render("dashboard", {
+      title: "Dashboard",
+      style: "stylesheets/dashboard.css",
+      item,
+    });
+  },
+};
 
 module.exports = dashbordController;
